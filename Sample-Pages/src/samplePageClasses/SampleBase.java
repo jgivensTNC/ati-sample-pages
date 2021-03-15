ee1065d2efa7570aa778c12157de8eb8b33d0050
jgivens <jgivens@TC-OLD-LT-159.technatomy.local>
2021-03-15 18:01:03 -0400

    Created SampleBase class
    ATI-1

---
+++ Sample-Pages/src/samplePageClasses/SampleBase.java
@@ -0,0 +1,26 @@
+package samplePageClasses;
+
+import platformIndependentCore.core.AutomatedPage;
+import platformIndependentCore.scripts.TestScript;
+
+/**
+ * <b>Name   :</b> SampleBase.java
+ *<p>	 
+ * <b>Generated     :</b> Mar 15, 2021
+ *<p>
+ * <b>Description   :</b> 
+ *<p>
+ * 
+ * @since  Mar 15, 2021
+ * @author jgivens
+ */
+public class SampleBase extends AutomatedPage {
+
+	/**
+	 * 
+	 */
+	public SampleBase() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
