b5091ea8b3c6de631411a5cad770596f2e04d7c8
jgivens <jgivens@TC-OLD-LT-159.technatomy.local>
2021-03-16 13:01:22 -0400

    Added comment to constructor
    ATI-1

--- Sample-Pages/src/samplePageClasses/SampleBase.java
+++ Sample-Pages/src/samplePageClasses/SampleBase.java
@@ -1,23 +1,22 @@
 package samplePageClasses;
 
 import platformIndependentCore.core.AutomatedPage;
-import platformIndependentCore.scripts.TestScript;
 
 /**
- * <b>Name   :</b> SampleBase.java
- *<p>	 
- * <b>Generated     :</b> Mar 15, 2021
- *<p>
- * <b>Description   :</b> 
- *<p>
- * 
- * @since  Mar 15, 2021
+ * <b>Name :</b> SampleBase.java
+ * <p>
+ * <b>Generated :</b> Mar 15, 2021
+ * <p>
+ * <b>Description :</b>
+ * <p>
+ *
+ * @since Mar 15, 2021
  * @author jgivens
  */
 public class SampleBase extends AutomatedPage {
 
 	/**
-	 * 
+	 * Constructor
 	 */
 	public SampleBase() {
 		// TODO Auto-generated constructor stub
