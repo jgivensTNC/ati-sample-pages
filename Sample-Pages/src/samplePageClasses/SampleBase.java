65ebfe8ab2fcca5035c0e1aa0a112cc7198e4064
VBAAUSRUDISC <VBAAUSRUDISC@281-WSV-XQA07.vba.va.gov>
2021-03-17 11:11:54 -0400

    Updates the base class

--- Sample-Pages/src/samplePageClasses/SampleBase.java
+++ Sample-Pages/src/samplePageClasses/SampleBase.java
@@ -5,9 +5,9 @@ import platformIndependentCore.core.AutomatedPage;
 /**
  * <b>Name :</b> SampleBase.java
  * <p>
- * <b>Generated :</b> Mar 15, 2021
+ * <b>Generated :</b> Mar 17, 2021
  * <p>
- * <b>Description :</b>
+ * <b>Description : Added information about the page class</b>
  * <p>
  *
  * @since Mar 15, 2021
@@ -20,6 +20,15 @@ public class SampleBase extends AutomatedPage {
 	 */
 	public SampleBase() {
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	private String getMethod() {
+		System.out.println("Example");
+		return "example";
 	}
 
 }
