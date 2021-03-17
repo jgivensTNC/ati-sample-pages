022f35100b5005037b8ceca86c9ebe4f51c41844
VBAAUSRUDISC <VBAAUSRUDISC@281-WSV-XQA07.vba.va.gov>
2021-03-17 12:58:03 -0400

    Changes author and date on base class

--- Sample-Pages/src/samplePageClasses/SampleBase.java
+++ Sample-Pages/src/samplePageClasses/SampleBase.java
@@ -5,13 +5,13 @@ import platformIndependentCore.core.AutomatedPage;
 /**
  * <b>Name :</b> SampleBase.java
  * <p>
- * <b>Generated :</b> Mar 15, 2021
+ * <b>Generated :</b> Mar 13, 2021
  * <p>
  * <b>Description :</b>
  * <p>
  *
  * @since Mar 15, 2021
- * @author jgivens
+ * @author crudisill
  */
 public class SampleBase extends AutomatedPage {
 
