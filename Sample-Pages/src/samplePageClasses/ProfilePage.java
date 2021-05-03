d92f0249e613c6303bb8976e922e979384436b1e
jgivens <jgivens@TC-OLD-LT-159.technatomy.local>
2021-05-03 14:30:17 -0400

    updates from writing documentation

---
+++ Sample-Pages/src/samplePageClasses/ProfilePage.java
@@ -0,0 +1,60 @@
+package samplePageClasses;
+
+/**
+ * <b>Name :</b> ProfilePage.java
+ * <p>
+ * <b>Generated :</b> Apr 27, 2021
+ * <p>
+ * <b>Description :</b>
+ * <p>
+ *
+ * @since Apr 27, 2021
+ * @author jgivens
+ */
+public class ProfilePage extends SampleBase {
+
+	/**
+	 * @param url for the page
+	 */
+	public ProfilePage() {
+		super("www");
+
+	}
+
+	public void setFirstName() {
+
+	}
+
+	public void setLastName() {
+
+	}
+
+	public void selectPhoneType() {
+
+	}
+
+	public void setPhoneNumber() {
+
+	}
+
+	public void setFaxNumber() {
+
+	}
+
+	public void setStreetAddress() {
+
+	}
+
+	public void setCity() {
+
+	}
+
+	public void selectState() {
+
+	}
+
+	public void setZipCode() {
+
+	}
+
+}
